5ccd58904979c45c85e69ced3299e86003a93a46
harehare <hareha@naver.com>
2024-05-29 16:26:52 +0900

    Update PressablePagination.tsx

--- src/PressablePagination/PressablePagination.tsx
+++ src/PressablePagination/PressablePagination.tsx
@@ -33,6 +33,11 @@ const PressablePagination = (props: PressablePaginationProps) => {
             outputRange: props.indicatorColor,
             extrapolate: "clamp",
           });
+          const fontColor = props.scrollX.interpolate({
+            inputRange,
+            outputRange: ['#AFB8C1','white','#AFB8C1'],
+            extrapolate: "clamp",
+          });
           return (
             <TouchableOpacity
               key={idx}
@@ -48,13 +53,28 @@ const PressablePagination = (props: PressablePaginationProps) => {
                   backgroundColor,
                   props.inidicatorBorderRadius
                 )}
-              />
+              >
+                <Animated.Text style={fontColorStyle(fontColor)}>
+                  {_.title}
+                </Animated.Text>
+              </Animated.View>
+
             </TouchableOpacity>
           );
         })}
       </View>
     </>
   );
+};
+
+const fontColorStyle = (
+  color: Animated.AnimatedInterpolation<string | number>,
+): Animated.Animated => {
+  return {
+    color,
+    fontSize:17,
+    fontWeight:700
+  };
 };
 
 export default PressablePagination;
